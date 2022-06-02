f7a51321ae59e7c60bdf708a3a825824309b8d9c
doitauchamhet <dai15081999@gmail.com>
2022-06-02 21:09:48 +0700

    ff

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,12 @@
+const Menu = require("../../models/menu");
+
+function homeController() {
+  return {
+    async index(req, res) {
+     const products = await Menu.find()
+     return res.render("home", { products: products });
+    },
+  };
+}
+
+module.exports = homeController;
